3184a4c2a3607148bc7250b6a4a0c73f9cf61d6c
yasindu10 <yasindusamarasinghe10@gmail.com>
2024-06-28 12:36:36 +0530

    starting log in page development

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:project_incognito/components/custom_text_fields.dart';
 
 class LogInPage extends StatefulWidget {
   const LogInPage({super.key});
@@ -10,8 +12,41 @@ class LogInPage extends StatefulWidget {
 class _LogInPageState extends State<LogInPage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          DefaultTextField(
+            title: 'User Name',
+            controller: TextEditingController(),
+            icon: Icons.person_rounded,
+          ),
+          DefaultTextField(
+            title: 'Password',
+            controller: TextEditingController(),
+            icon: Icons.person_rounded,
+          ),
+          const Gap(15),
+          SizedBox(
+            width: size.width / 1.1,
+            height: 45,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blueAccent,
+              ),
+              onPressed: () {},
+              child: const Text(
+                'Log In',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
